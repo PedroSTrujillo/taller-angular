bc9f0f591a219a5654620ff82e73d4abc3befe75
Pedro Pablo S. Trujillo <123520249+PedroSTrujillo@users.noreply.github.com>
2024-10-24 21:37:18 -0500

    Crear module y service

---
+++ src/app/serie/serie.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Serie } from './serie';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SerieService {
+  private apiUrl: string = environment.baseUrl + 'series';
+  constructor(private http: HttpClient) {}
+  getSeries(): Observable<Serie[]> {
+    return this.http.get<Serie[]>(this.apiUrl);
+  }
+}
